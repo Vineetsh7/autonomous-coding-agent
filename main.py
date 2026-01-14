a55dff83876548aa179787a8c8c3883851015431
Vineetsh7 <xyz.meoutlook010@gmail.com>
2026-01-15 00:27:47 +0530

    enhanced the print property

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from client.llm_client import LLMClient
 async def main():
     client = LLMClient()
     messages = [{"role": "user", "content": "What's up"}] 
-    async for event in client.chat_completion(messages, False):
+    async for event in client.chat_completion(messages, True):
         print(event)
     print("done")
 
